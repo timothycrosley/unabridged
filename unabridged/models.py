0916891650c9e451b2615c12cf7e71bb502947a1
Timothy Crosley <timothy.crosley@gmail.com>
2021-01-02 15:03:37 -0800

    Add initial models

---
+++ unabridged/models.py
@@ -0,0 +1,20 @@
+from tortoise.models import Model
+from tortoise import fields
+
+from enum import IntEnum
+
+Reaction = IntEnum("Reaction", "NONE HATED DISLIKED INDIFFERENT LIKED LOVED")
+
+
+class Event(Model):
+    id = fields.IntField(pk=True)
+    kind = fields.CharField(200)
+    name = fields.CharField(200)
+    description = fields.TextField(default="")
+    comments = fields.TextField(default="")
+    reaction: Reaction = fields.IntEnumField(Reaction, default=Reaction.NONE)
+    created = fields.DatetimeField(auto_now_add=True)
+
+
+class State(Event):
+    completed = fields.DatetimeField()

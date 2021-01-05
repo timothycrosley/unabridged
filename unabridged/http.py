547bd3e9472ec462efd4330a8f7db3b3785f925e
Timothy Crosley <timothy.crosley@gmail.com>
2021-01-04 23:23:57 -0800

    Fix references to model

--- unabridged/http.py
+++ unabridged/http.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel
 from tortoise.contrib.fastapi import HTTPNotFoundError, register_tortoise
 
 from .pydantic_models import Event, EventIn
+from . import models
 
 app = FastAPI(title="Unabridged activity API")
 
@@ -15,31 +16,31 @@ class Status(BaseModel):
 
 @app.get("/events", response_model=List[Event])
 async def get_events():
-    return await Event.from_queryset(Event.all())
+    return await Event.from_queryset(models.Event.all())
 
 
 @app.post("/events", response_model=Event)
 async def create_event(event: EventIn):
-    event_obj = await Event.create(**event.dict(exclude_unset=True))
+    event_obj = await models.Event.create(**event.dict(exclude_unset=True))
     return await Event.from_tortoise_orm(event_obj)
 
 
 @app.get("/event/{event_id}", response_model=Event, responses={404: {"model": HTTPNotFoundError}})
 async def get_event(event_id: int):
-    return await Event.from_queryset_single(Event.get(id=event_id))
+    return await Event.from_queryset_single(models.Event.get(id=event_id))
 
 
 @app.put("/event/{user_id}", response_model=Event, responses={404: {"model": HTTPNotFoundError}})
 async def update_event(event_id: int, event: EventIn):
-    await Event.filter(id=event_id).update(**event.dict(exclude_unset=True))
-    return await Event.from_queryset_single(Event.get(id=event_id))
+    await models.Event.filter(id=event_id).update(**event.dict(exclude_unset=True))
+    return await Event.from_queryset_single(models.Event.get(id=event_id))
 
 
 @app.delete(
     "/event/{event_id}", response_model=Status, responses={404: {"model": HTTPNotFoundError}}
 )
 async def delete_event(event_id: int):
-    deleted_count = await Event.filter(id=event_id).delete()
+    deleted_count = await models.Event.filter(id=event_id).delete()
     if not deleted_count:
         raise HTTPException(status_code=404, detail=f"Event {event_id} not found")
     return Status(message=f"Deleted event {event_id}")

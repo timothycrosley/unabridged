ffee78a6398139f5cd1e6ea5943f2c2e51da13eb
Timothy Crosley <timothy.crosley@gmail.com>
2021-01-05 23:57:23 -0800

    Support both state and event updates

--- unabridged/http.py
+++ unabridged/http.py
@@ -13,37 +13,40 @@ app = FastAPI(title="Unabridged activity API")
 class Status(BaseModel):
     message: str
 
-
-@app.get("/events", response_model=List[Event])
-async def get_events():
-    return await Event.from_queryset(models.Event.all())
-
-
-@app.post("/events", response_model=Event)
-async def create_event(event: EventIn):
-    event_obj = await models.Event.create(**event.dict(exclude_unset=True))
-    return await Event.from_tortoise_orm(event_obj)
+for model_name, Model, InModel, DBModel in (
+    ('event', Event, EventIn, models.Event),
+    ('state', State, StateIn, models.State),
+):
+    @app.get(f"/{model_name}s", response_model=List[Model])
+    async def get_all_():
+        return await Model.from_queryset(DBModel.all())
 
 
-@app.get("/event/{event_id}", response_model=Event, responses={404: {"model": HTTPNotFoundError}})
-async def get_event(event_id: int):
-    return await Event.from_queryset_single(models.Event.get(id=event_id))
+    @app.post(f"/{model_name}s", response_model=Model)
+    async def create_(event: InModel):
+        event_obj = await DBModel.create(**event.dict(exclude_unset=True))
+        return await Model.from_tortoise_orm(event_obj)
 
 
-@app.put("/event/{user_id}", response_model=Event, responses={404: {"model": HTTPNotFoundError}})
-async def update_event(event_id: int, event: EventIn):
-    await models.Event.filter(id=event_id).update(**event.dict(exclude_unset=True))
-    return await Event.from_queryset_single(models.Event.get(id=event_id))
+    @app.get(f"/{model_name}/{{event_id}}", response_model=Model, responses={404: {"model": HTTPNotFoundError}})
+    async def get_(event_id: int):
+        return await Model.from_queryset_single(DBModel.get(id=event_id))
 
 
-@app.delete(
-    "/event/{event_id}", response_model=Status, responses={404: {"model": HTTPNotFoundError}}
-)
-async def delete_event(event_id: int):
-    deleted_count = await models.Event.filter(id=event_id).delete()
-    if not deleted_count:
-        raise HTTPException(status_code=404, detail=f"Event {event_id} not found")
-    return Status(message=f"Deleted event {event_id}")
+    @app.put(f"/{model_name}/{{user_id}}", response_model=Model, responses={404: {"model": HTTPNotFoundError}})
+    async def update_(event_id: int, event: InModel):
+        await models.Model.filter(id=event_id).update(**event.dict(exclude_unset=True))
+        return await Model.from_queryset_single(DBModel.get(id=event_id))
+
+
+    @app.delete(
+        f"/{model_name}/{{event_id}}", response_model=Status, responses={404: {"model": HTTPNotFoundError}}
+    )
+    async def delete_(event_id: int):
+        deleted_count = await DBModel.filter(id=event_id).delete()
+        if not deleted_count:
+            raise HTTPException(status_code=404, detail=f"Event {event_id} not found")
+        return Status(message=f"Deleted event {event_id}")
 
 
 register_tortoise(

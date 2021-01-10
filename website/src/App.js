ad8a3796e123bb588dac1e637403d6550ed4414e
Timothy Crosley <timothy.crosley@gmail.com>
2021-01-09 23:03:49 -0800

    initial steps to use react bootstrap

--- website/src/App.js
+++ website/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button, Container, Row, Col} from 'react-bootstrap/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+         <Button text="test button" />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
